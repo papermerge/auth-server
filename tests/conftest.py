75c525ddfe3f35423d1e3f3ec386692269428697
Eugen Ciur <eugen@papermerge.com>
2023-05-29 10:29:26 +0200

    better conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,34 @@
 import logging
 import pytest
 
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import Session
 
-from auth_server.database import Base, engine
+from auth_server.database import Base
 
 logger = logging.getLogger(__name__)
 
 
+@pytest.fixture()
+def db_engine():
+    engine = create_engine("sqlite+pysqlite:///:memory:")
+    yield engine
+
+
 @pytest.fixture(autouse=True)
-def setup_db_schema():
-    Base.metadata.create_all(engine)
+def setup_db_schema(db_engine: Engine):
+    Base.metadata.create_all(db_engine)
     yield
-    Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(db_engine)
 
 
 @pytest.fixture()
-def db_connection():
-    with engine.begin() as conn:
+def db_connection(db_engine: Engine):
+    with db_engine.begin() as conn:
         yield conn
 
 
 @pytest.fixture()
-def db_session():
-    with Session(engine) as session:
+def db_session(db_engine: Engine):
+    with Session(db_engine) as session:
         yield session
