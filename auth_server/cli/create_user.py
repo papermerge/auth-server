f4fc62700e1516e3832449db4356d7b3aebce080
Eugen Ciur <eugen@papermerge.com>
2023-09-01 08:03:33 +0200

    include set_password utility

--- auth_server/cli/create_user.py
+++ auth_server/cli/create_user.py
@@ -1,34 +1,38 @@
 import click
+import logging
 
 from sqlalchemy.orm import sessionmaker
 from auth_server.database.engine import engine
-from auth_server.crud import create_user
+from auth_server.crud import create_user, get_user_by_username
+
+
+logger = logging.getLogger(__name__)
 
 
 @click.command()
 @click.option(
-    '--username',
-    prompt='Username',
-)
-@click.option(
-    '--email',
-    prompt='Email',
-)
-@click.option(
-    '--password',
-    prompt='Password',
-    hide_input=True,
-    confirmation_prompt=True
-)
-def cli(username: str, email: str, password: str):
+    '--username', envvar='PAPERMERGE__AUTH__USERNAME')
+@click.option('--email', envvar='PAPERMERGE__AUTH__EMAIL')
+@click.option('--password', envvar='PAPERMERGE__AUTH__PASSWORD')
+def cli(username: str, email: str | None, password: str):
     SessionLocal = sessionmaker(engine)
     db = SessionLocal()
-    create_user(
-        db,
-        username=username,
-        password=password,
-        email=email
-    )
+
+    if not email:
+        email = f'{username}@example.com'
+
+    user = get_user_by_username(db, username)
+    if user:
+        logger.warning(f"User '{username}' already exists.")
+    else:
+        create_user(
+            db,
+            username=username,
+            password=password,
+            email=email
+        )
+        logger.info(f"User '{username}' created.")
+
     db.close()
 
 
---
+++ auth_server/cli/set_password.py
@@ -0,0 +1,38 @@
+import click
+import logging
+
+from sqlalchemy.orm import sessionmaker
+from auth_server.database.engine import engine
+from auth_server.crud import get_user_by_username
+from passlib.hash import pbkdf2_sha256
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option('--username', prompt='username')
+@click.option(
+    '--password',
+    prompt='password',
+    hide_input=True,
+    confirmation_prompt=True
+)
+def cli(username: str, password: str):
+    SessionLocal = sessionmaker(engine)
+    db = SessionLocal()
+
+    user = get_user_by_username(db, username)
+    if not user:
+        print("User not found")
+        return
+
+    db.add(user)
+    user.password = pbkdf2_sha256.hash(password)
+    db.commit()
+
+    db.close()
+
+
+if __name__ == '__main__':
+    cli()
