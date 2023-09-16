165ea242c601ea3f19b52db11fd527460e57e9c4
Eugen Ciur <eugen@papermerge.com>
2023-09-16 06:46:36 +0200

    better logging

--- auth_server/auth.py
+++ auth_server/auth.py
@@ -71,17 +71,25 @@ def create_access_token(
     algorithm: str,
     expires_delta: timedelta | None = None
 ) -> str:
+    logger.debug(f"create access token for data={data}")
+
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(
-        to_encode,
-        secret_key,
-        algorithm=algorithm
-    )
+
+    try:
+        encoded_jwt = jwt.encode(
+            to_encode,
+            secret_key,
+            algorithm=algorithm
+        )
+    except Exception as exc:
+        logger.error(exc)
+        raise
+
     return encoded_jwt
 
 
