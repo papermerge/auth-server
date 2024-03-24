1beae97dd37382e788056124070ca72535d0abb5
Eugen Ciur <eugen@papermerge.com>
2024-03-24 07:06:09 +0100

    add verify endpoint

--- auth_server/utils.py
+++ auth_server/utils.py
@@ -1,3 +1,11 @@
+from fastapi import Request, FastAPI
+from fastapi.security.utils import get_authorization_scheme_param
+
+from .config import Settings
+
+app = FastAPI()
+settings = Settings()
+
 
 def raise_on_empty(**kwargs):
     """Raises ValueError exception if at least one value of the
@@ -8,3 +16,22 @@ def raise_on_empty(**kwargs):
             raise ValueError(
                  f"{key} is expected to be non-empty"
             )
+
+
+def from_header(request: Request) -> str | None:
+    authorization = request.headers.get("Authorization")
+    scheme, token = get_authorization_scheme_param(authorization)
+
+    if not authorization or scheme.lower() != "bearer":
+        return None
+
+    return token
+
+
+def from_cookie(request: Request) -> str | None:
+    cookie_name = settings.papermerge__security__cookie_name
+    return request.cookies.get(cookie_name, None)
+
+
+def get_token(request: Request) -> str | None:
+    return from_cookie(request) or from_header(request)
