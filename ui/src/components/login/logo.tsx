83d915c59151c644927fd03b55bf4ae414fd99f8
Eugen Ciur <eugen@papermerge.com>
2024-11-16 08:55:46 +0100

    it works!

--- ui/src/components/login/logo.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Logo() {
-  return (
-    <div className="logo m-2">
-      <img alt="Papermerge logo" width="112" height="108" src="/images/papermerge4.svg" />
-    </div>
-  );
-}
--- ui/src/components/login/types.ts
+++
@@ -1,9 +0,0 @@
-export type InputProps = {
-  onchange: (value: string) => void;
-  type: "text" | "password";
-  name: string;
-  placeholder: string;
-}
-
-
-export type ClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>;
