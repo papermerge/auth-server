c18c71137a2db90e862c3be682866efff3660a03
Eugen Ciur <ciur.eugen@gmail.com>
2025-04-23 05:44:31 +0200

    fix failing yarn build command

--- ui2/src/main.tsx
+++ ui2/src/main.tsx
@@ -3,6 +3,10 @@ import App from './App';
 import { initializeI18n } from "./initializeI18n";
 
 
-await initializeI18n()
+async function start_app() {
+  await initializeI18n()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+}
+
+start_app()
